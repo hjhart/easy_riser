761417350eb0e0e8b7fd391bf35705df0444efd1
James Hart <hjhart@gmail.com>
2011-02-27 14:01:52 -0800

    Created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyRiser::Application.config.secret_token = '6173d84da339b4230f8bca65d67737ce755cf0efbf2b5aab0b18eaa4e8ca28eca19c83beb3daf6be3a115c365caf6851905597f1044cc9f6a206103e35ce7861'
